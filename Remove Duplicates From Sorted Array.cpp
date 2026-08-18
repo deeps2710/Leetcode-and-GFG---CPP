e64a34ba606b6832d45b33bad96d9f3b3ffffdd5
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-18 10:03:36 +0530

    Implement second method for removing duplicates
    Added a second method using unordered_map to remove duplicates.

--- Remove Duplicates From Sorted Array.cpp
+++ Remove Duplicates From Sorted Array.cpp
@@ -1,9 +1,27 @@
+//Method 1
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int k=1;
         for(int i=1; i<nums.size(); i++){
             if(nums[i]!=nums[i-1]){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
+
+//Method 2
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int k=0;
+        for(int i=0; i<nums.size(); i++){
+            if(mp.find(nums[i])==mp.end()){
+                mp[nums[i]]++;
                 nums[k]=nums[i];
                 k++;
             }
