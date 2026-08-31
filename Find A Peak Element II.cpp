cd9b1d78b042e19518e07e7e9991bd61956c659f
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-31 19:38:04 +0530

    Implement findPeakGrid function in Solution class

---
+++ Find A Peak Element II.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        int low=0;
+        int high=n-1;
+        
+        while(low<=high){
+            int mid=(low+high)/2;
+            int maxRow=0;
+            for(int i=1;i<m;i++){
+                if(mat[i][mid]>mat[maxRow][mid]){
+                    maxRow=i;
+                }
+            }
+            int left=-1;
+            int right=-1;
+
+            if(mid-1>=0){
+                left=mat[maxRow][mid-1];
+            }
+            if(mid+1<n){
+                right=mat[maxRow][mid+1];
+            }
+
+            if(mat[maxRow][mid]>left && mat[maxRow][mid]>right){
+                return {maxRow,mid};
+            }
+
+            if(left>mat[maxRow][mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return {-1,-1};
+    }
+};
