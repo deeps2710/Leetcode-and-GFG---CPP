f29747d0f626dd833ae215700f5520e1fb424d47
Deepshikha Rani <deepshikharani297@gmail.com>
2026-09-08 10:12:51 +0530

    Implement second approach for first repeated character
    Added a second approach using a frequency array to find the first repeated character.

--- Repeated Character.cpp
+++ Repeated Character.cpp
@@ -1,12 +1,25 @@
 class Solution {
   public:
     char firstRep(string s) {
+        // Approach 1: HashMap
         unordered_map<char, int> freq;
         for(char ch: s){
             freq[ch]++;
         }
         for(char ch: s){
             if(freq[ch]>1){
+                return ch;
+            }
+        }
+        return '#';
+        
+        //Approach 2: Frequency Array
+        vector<int> freq(26,0);
+        for(char ch: s){
+            freq[ch-'a']++;
+        }
+        for(char ch: s){
+            if (freq[ch-'a']>1){
                 return ch;
             }
         }
