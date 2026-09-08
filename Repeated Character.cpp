264618f65b407597fc1e9e21372eeaf191094b57
Deepshikha Rani <deepshikharani297@gmail.com>
2026-09-08 09:34:31 +0530

    Implement firstRep function to find first repeated char

---
+++ Repeated Character.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    char firstRep(string s) {
+        unordered_map<char, int> freq;
+        for(char ch: s){
+            freq[ch]++;
+        }
+        for(char ch: s){
+            if(freq[ch]>1){
+                return ch;
+            }
+        }
+        return '#';
+    }
+};
