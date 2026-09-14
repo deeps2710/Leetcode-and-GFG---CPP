7823fde4b3209ace2121a9f2f2889179631644b8
Deepshikha Rani <deepshikharani297@gmail.com>
2026-09-14 19:17:58 +0530

    Implement function to count numbers with even digits

---
+++ Find Numbers With Even Number Of Digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int ans=0;
+        for(int i: nums){
+            int count=0;
+            while(i>0){
+                i=i/10;
+                count++;
+            }
+            if(count%2==0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
