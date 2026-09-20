9994d19149469fcbe760a721c4b5d700f20fec98
Deepshikha Rani <deepshikharani297@gmail.com>
2026-09-20 18:15:13 +0530

    Implement sorting of array by parity II

---
+++ Sort Array By Parity II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int even=0;
+        int odd=1;
+        while(even<nums.size() && odd<nums.size()){
+            if(nums[even]%2==0){
+                even+=2;
+            }
+            else if(nums[odd]%2!=0){
+                odd+=2;
+            }else{
+                int temp=nums[even];
+                nums[even]=nums[odd];
+                nums[odd]=temp;
+            }
+        }
+        return nums;
+    }
+};
